b591d6fdd0d5999341e591cc6f4783532bf830f1
Stefan Naewe <stefan.naewe@gmail.com>
2017-08-31 09:34:38 +0200

    after initial import in AtmelStudio
    Signed-off-by: Stefan Naewe <stefan.naewe@gmail.com>

---
+++ FsmMultitasking/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 31.08.2017 09:30:38
+ * Author : stefan
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
